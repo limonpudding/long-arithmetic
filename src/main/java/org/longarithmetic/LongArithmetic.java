2e47fc36105c424012588e32283fab7902a84027
limonpudding <martyshenko.sergei@yandex.ru>
2018-08-02 11:38:20 +0500

    Добавленая функция сложения

--- src/main/java/org/longarithmetic/LongArithmetic.java
+++ src/main/java/org/longarithmetic/LongArithmetic.java
@@ -7,7 +7,24 @@ public class LongArithmetic {
 
     }
 
+    public LongArithmetic() {
+        length = 0;
+    }
+
     private int getLength() {
         return length;
+    }
+
+    public static LongArithmetic Sum(LongArithmetic a, LongArithmetic b) {
+        LongArithmetic result = new LongArithmetic();
+        int maxLenght = a.getLength() > b.getLength()? a.getLength() : b.getLength();
+        int tmp = 0;
+        int i;
+        for (i = 0; i < maxLenght; ++i) {
+            result.digits[0] = (byte)((a.digits[0] + b.digits[0] + tmp) % 10);
+            tmp = (a.digits[0] + b.digits[0] + tmp) / 10;
+        }
+        result.digits[++i] = (byte)tmp;
+        return result;
     }
 }
