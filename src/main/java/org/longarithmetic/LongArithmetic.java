7b2c6663976a567b3d356bb1c54f9f95f3468a29
Tararkov Andrey <gfhflbuvf46@mail.ru>
2018-08-02 11:35:47 +0500

    Добавлен параметр для изменения длины числа Добавлена функция вычисления длины числа

--- src/main/java/org/longarithmetic/LongArithmetic.java
+++ src/main/java/org/longarithmetic/LongArithmetic.java
@@ -1,7 +1,8 @@
 package org.longarithmetic;
 
 public class LongArithmetic {
-    private static byte []digits = new byte[10000];
+    private static int n;//максимальная длина числа
+    private static byte []digits = new byte[n];
     private static int length = 0;
     public LongArithmetic(String number) {
         length=number.length();
@@ -11,6 +12,10 @@ public class LongArithmetic {
     }
 
     private int GetLength() {
+        int i;
+        for (i = n - 1; i >= 0 && digits[i] == 0; --i);
+        length = i;
         return length;
     }
+
 }
