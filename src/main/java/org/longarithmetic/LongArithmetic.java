3f0db86ba32ae39434e086463e17c5aff23b2a42
Tararkov Andrey <gfhflbuvf46@mail.ru>
2018-08-02 11:23:50 +0500

    Добавлен конструктор (заполняет массив с числом и длину)

--- src/main/java/org/longarithmetic/LongArithmetic.java
+++ src/main/java/org/longarithmetic/LongArithmetic.java
@@ -4,10 +4,13 @@ public class LongArithmetic {
     private static byte []digits = new byte[10000];
     private static int length = 0;
     public LongArithmetic(String number) {
-
+        length=number.length();
+        int j=0;
+        for (int i = length - 1; i >= 0; --i)
+            digits[j++]=(byte)number.charAt(i);
     }
 
-    private int getLength() {
+    private int GetLength() {
         return length;
     }
 }
