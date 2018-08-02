8f26a5cefa479e0f506318e08948caed150101d0
limonpudding <martyshenko.sergei@yandex.ru>
2018-08-02 10:58:38 +0500

    Добавлен минимальный шаблон класса длинной арифметики

---
+++ src/main/java/org/longarithmetic/LongArithmetic.java
@@ -0,0 +1,13 @@
+package org.longarithmetic;
+
+public class LongArithmetic {
+    private static byte []digits = new byte[10000];
+    private static int length = 0;
+    public LongArithmetic(String number) {
+
+    }
+
+    private int getLength() {
+        return length;
+    }
+}
