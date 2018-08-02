fb4f1198a133ae7caae0ffa558e7f79218319b9b
Tararkov Andrey <gfhflbuvf46@mail.ru>
2018-08-02 13:17:03 +0500

    Добавлен метод Clean() для очистки содержимого объекта

--- src/main/java/org/longarithmetic/LongArithmetic.java
+++ src/main/java/org/longarithmetic/LongArithmetic.java
@@ -27,6 +27,7 @@ public class LongArithmetic {
     private void Clean(){
         for (int i=0;i<n;++i)
             digits[i]=0;
+        length = 0;
     }
 
     public static LongArithmetic Sum(LongArithmetic a, LongArithmetic b) {
