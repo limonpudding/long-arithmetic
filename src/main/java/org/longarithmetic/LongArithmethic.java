bd033c3767d467c9b5cd6484690ea4945c031b3b
Tararkov Andrey <gfhflbuvf46@mail.ru>
2018-08-06 11:26:54 +0500

    Добавлена реализация списков, изменён метод взятия значений из массивов в lAMath

--- src/main/java/org/longarithmetic/LongArithmethic.java
+++ src/main/java/org/longarithmetic/LongArithmethic.java
@@ -3,15 +3,11 @@ package org.longarithmetic;
 public interface LongArithmethic extends Comparable<LongArithmethic> {
     byte[] getDigits();
 
-    void setDigits(byte[] digits);
-
     void setLength(int length);
 
     byte getDigit(int index);
 
     void setDigit(byte digit, int index);
-
-    void clean();
 
     int getLength();
 
